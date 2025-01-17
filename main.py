33d59223f049c69a8031d1de85bf18cb0b03b996
AryanJadile <aryanjadile@gmail.com>
2025-01-17 13:00:09 +0530

    random guess done

---
+++ main.py
@@ -0,0 +1,14 @@
+import random
+
+fruits = [
+    "Apple", "Banana", "Cherry", "Date", "Fig", 
+    "Grapes", "Mango", "Orange", "Pineapple", "Strawberry"
+]
+
+
+guess = random.choice(wordlist)
+print(guess)
+
+#Hint
+
+print(guess[0])
