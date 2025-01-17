54f37324c7521a2a85aa9b17023dd47ace27d97c
AryanJadile <aryanjadile@gmail.com>
2025-01-17 14:18:08 +0530

    update hint

---
+++ hint.py
@@ -0,0 +1,30 @@
+fruits = [
+    "Apple", "Banana", "Cherry", "Peach", "Watermelon"
+    "Grapes", "Mango", "Orange", "Pineapple", "Strawberry"
+]
+
+def hint(guess):
+    if guess in fruits:
+        if guess == "Apple":
+            print("I keep the doctors away!")
+        elif guess == "Banana":
+            print("Slip on me if you're not careful, but I’m delicious inside!")
+        elif guess == "Cherry":
+            print("I’m small, red, and on top of your sundae!!")
+        elif guess == "Peach":
+            print("I’m soft, sweet, and blushing like a princess at the royal ball—Cinderella would totally pick me!")
+        elif guess == "Watermelon":
+            print("I’m big, juicy, and full of water—perfect for a summer day!")
+        elif guess == "Grapes":
+            print("I hang out in bunches, sometimes I turn into wine, cheers!")
+        elif guess == "Mango":
+            print("King of the fruits they call me, but I come only in summers!!")
+        elif guess=="Orange":
+            print("Peel me, juice me, but don’t call me the color of my own name!")
+        elif guess == "Pineapple":
+            print("I’m spiky on the outside, but once you peel me, I’m sweet and tropical inside!")
+        elif guess == "Strawberry":
+            print("I wear my seeds on the outside, fashion-forward and delicious!")
+    else:
+        return None
+
